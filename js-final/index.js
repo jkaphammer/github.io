47bccffe4d0e27c283cf0e568e1a49d256bf68be
Jenn Kaphammer <jeka7802@colorado.edu>
2023-12-09 18:30:40 -0700

    Snake form functionality

--- js-final/index.js
+++ js-final/index.js
@@ -1,5 +1,3 @@
-
-
 var canvas = document.getElementById('game');
 var context = canvas.getContext('2d');
 
@@ -7,7 +5,7 @@ var context = canvas.getContext('2d');
 // (e.g. 16 * 25 = 400)
 var grid = 16;
 var count = 0;
-
+var numberHold = "";
 var snake = {
   x: 160,
   y: 160,
@@ -22,20 +20,62 @@ var snake = {
   // length of the snake. grows when eating an apple
   maxCells: 4
 };
-var apple = {
-  x: 320,
-  y: 320
-};
+// var apple = {
+//   x: 320,
+//   y: 320,
+//   number: 0 // Initial number on the apple
 
+// };
+var apples = [
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 0},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 1},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 2},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 3},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 4},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 5},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 6},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 7},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 8},
+  {x: getRandomInt(0, 25) * grid, y: getRandomInt(1, 25) * grid, number: 9},
+  // add more apples as needed
+];
+function submitPhoneNumber(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  var submittedPhoneNumber = document.getElementById('phoneNumber').value;
+  alert('Submitted Phone Number: ' + submittedPhoneNumber);
+}
+function getRandomAppleInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
+function appleNumber(){
+  apple.number = getRandomAppleInclusive(0, 9);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillStyle = 'red';
+}
+var text = getRandomAppleInclusive(0, 9);
+context.fillStyle = 'white';
+context.font = '20px Arial';
 // get random whole numbers in a specific range
 // @see https://stackoverflow.com/a/1527820/2124254
 function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
+
 // game loop
 function loop() {
   requestAnimationFrame(loop);
+
+  if (numberHold.length >= 10){
+    alert("You submitted the phone number " + numberHold + ". Hope it was right!");
+    console.log("You submitted " + numberHold +" !");
+    numberHold = '';
+    document.getElementById('phone').value = '';
+  }
 
   // slow game loop to 15 fps instead of 60 (60/15 = 4)
   if (++count < 4) {
@@ -53,15 +93,28 @@ function loop() {
   // check if snake hit wall
   if (snake.x < 0 || snake.x >= canvas.width || snake.y < 0 || snake.y >= canvas.height){
     // if hit wall, reset the game
+    console.log("WALL COLLISION");
     snake.x = 160;
     snake.y = 160;
     snake.cells = [];
     snake.maxCells = 4;
     snake.dx = grid;
     snake.dy = 0;
+    // reset phone number form
+    numberHold = "";
+    document.getElementById('phone').value = "";
+    // apple.x = getRandomInt(0, 25) * grid;
+    // apple.y = getRandomInt(1, 25) * grid;
+    apples.forEach(function(apple, appleIndex) {
+      apples.splice(appleIndex, 1);
+      // add new apple at random position
 
-    apple.x = getRandomInt(0, 25) * grid;
-    apple.y = getRandomInt(0, 25) * grid;
+      apples.push({
+        x: getRandomInt(0, 25) * grid,
+        y: getRandomInt(1, 25) * grid,
+        number: apple.number
+      });
+    });
 
   }
 
@@ -91,39 +144,80 @@ function loop() {
   }
 
   // draw apple
-  context.fillStyle = 'red';
-  context.fillRect(apple.x, apple.y, grid-1, grid-1);
+  apples.forEach(function(apple) {
+    context.fillStyle = 'red';
+    context.fillRect(apple.x, apple.y, grid-1, grid-1);
+    context.fillStyle = 'white';
+    context.fillText(apple.number, apple.x, apple.y, grid-1, grid-1);
+  });
+
+  // var text = 'Hello, World!';
+  // context.fillStyle = 'white';
+  // context.font = '20px Arial';
+  // context.fillText(text, x + 10, y + 30);
 
   // draw snake one cell at a time
-  context.fillStyle = 'green';
+  context.fillStyle = "#C8D96F"; // mindaro
   snake.cells.forEach(function(cell, index) {
 
     // drawing 1 px smaller than the grid creates a grid effect in the snake body so you can see how long it is
     context.fillRect(cell.x, cell.y, grid-1, grid-1);
 
     // snake ate apple
-    if (cell.x === apple.x && cell.y === apple.y) {
-      snake.maxCells++;
-
-      // canvas is 400x400 which is 25x25 grids
-      apple.x = getRandomInt(0, 25) * grid;
-      apple.y = getRandomInt(0, 25) * grid;
-    }
-
+    apples.forEach(function(apple, appleIndex) {
+      if (cell.x === apple.x && cell.y === apple.y) {
+        snake.maxCells++;
+        if (numberHold.length < 10){ // makes sure that the form can only hold a valid 10-digits phone number
+          numberHold += apple.number.toString(); // saves the number eaten by snake
+          // text = getRandomAppleInclusive(0,9); // if apple is eaten, change the number on the apple to a different number
+          document.getElementById('phone').value = numberHold.toString(); // put the previous input into the form
+        }
+        if (numberHold.length >= 10){
+          alert("You submitted the phone number " + numberHold + ". Hope it was right!");
+          console.log("You submitted " + numberHold +" !");
+          numberHold = '';
+          document.getElementById('phone').value = '';
+        }
+        console.log("OUT OF IF " + numberHold);
+        // canvas is 400x400 which is 25x25 grids
+        // remove eaten apple from array
+        apples.splice(appleIndex, 1);
+        // add new apple at random position
+        apples.push({
+          x: getRandomInt(0, 25) * grid,
+          y: getRandomInt(1, 25) * grid,
+          number: apple.number
+        });
+      }
+    });
     // check collision with all cells after this one (modified bubble sort)
     for (var i = index + 1; i < snake.cells.length; i++) {
 
       // snake occupies same space as a body part. reset game
       if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        console.log("SELF COLLISION");
         snake.x = 160;
         snake.y = 160;
         snake.cells = [];
         snake.maxCells = 4;
         snake.dx = grid;
         snake.dy = 0;
+        // reset phone number form
+        numberHold = ""; 
+        document.getElementById('phone').value = "";
 
-        apple.x = getRandomInt(0, 25) * grid;
-        apple.y = getRandomInt(0, 25) * grid;
+        // apple.x = getRandomInt(0, 25) * grid;
+        // apple.y = getRandomInt(1, 25) * grid;
+        apples.forEach(function(apple, appleIndex) {
+          apples.splice(appleIndex, 1);
+          // add new apple at random position
+
+          apples.push({
+            x: getRandomInt(0, 25) * grid,
+            y: getRandomInt(1, 25) * grid,
+            number: apple.number
+          });
+        });
       }
     }
   });
@@ -161,14 +255,14 @@ document.addEventListener('keydown', function(e) {
 // start the game
 requestAnimationFrame(loop);
 
-// format phone number input
-document.getElementById('phone').addEventListener('input', function (event) {
-    let input = event.target;
-    let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
+// // format phone number input
+// document.getElementById('phone').addEventListener('input', function (event) {
+//     let input = event.target;
+//     let value = input.value.replace(/\D/g, ''); // Remove non-numeric characters
 
-    if (value.length > 0) {
-        value = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(6, 10)}`;
-    }
+//     if (value.length > 0) {
+//         value = `(${value.slice(0, 3)}) ${value.slice(3, 6)}-${value.slice(6, 10)}`;
+//     }
 
-    input.value = value;
-});
+//     input.value = value;
+// });
